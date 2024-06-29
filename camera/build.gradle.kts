d19a04b32e2f8c5152cf5138792159f93e3b4cf2
dohyun <dnlwkem39@gmail.com>
2024-06-29 12:30:55 +0900

    dependency bundle 생성

--- camera/build.gradle.kts
+++ camera/build.gradle.kts
@@ -68,10 +68,5 @@ dependencies {
     implementation(libs.androidx.paging.common)
     implementation(libs.androidx.paging.compose)
     implementation(libs.androidx.compose.material)
-    implementation(libs.androidx.camera.core)
-    implementation(libs.androidx.camera.lifecycle)
-    implementation(libs.androidx.camera.camera2)
-    implementation(libs.androidx.camera.video)
-    implementation(libs.androidx.camera.view)
-    implementation(libs.androidx.camera.extensions)
+    implementation(libs.bundles.androidx.camera)
 }
