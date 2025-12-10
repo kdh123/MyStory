e01f56e11c24cc8374fe132da7a54ad2782f621b
dohyun <dnlwkem39@gmail.com>
2025-12-10 15:24:31 +0900

    앱 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.dhkim.timecapsule"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.0.8"
+        versionCode = 17
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
